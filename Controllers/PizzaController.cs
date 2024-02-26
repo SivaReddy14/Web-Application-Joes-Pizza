a33ed1db4caa5d0a1175be0a7c1bcd54b5f94461
sivareddy <sivas14910@gmail.comgit config --global user.email sivas14910@gmail.comgit config --global user.email sivas14910@gmail.com>
2024-02-26 13:08:50 +0530

    AllFiles

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppJoesPizza.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
